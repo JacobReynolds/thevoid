ea8e8d7f7fc29886befd78b0efacad68882945e5
jasper <reyno511@umn.edu>
2016-05-09 22:19:29 -0500

    first version completely working

--- public/index.js
+++ public/index.js
@@ -1,5 +1,5 @@
 var socket = io();
-
+var id = 0;
 // Sends a chat message
 function shout() {
 	var message = $('#shout').val();
@@ -13,11 +13,76 @@ function shout() {
 	}
 }
 
-socket.on('new message', function (data) {
-	alert(data.message);
+//When enter is pressed, shout!
+$(".shout").keypress(function (e) {
+	if ((e.which && e.which == 13) || (e.keyCode && e.keyCode == 13)) {
+		shout();
+	}
 });
+
+socket.on('new message', function (data) {
+	thevoid(data.message);
+});
+
+function thevoid(message) {
+	$('.void').append('<span class="echo" id="' + id + '">' + message + '</span>');
+	//Animation calculations
+	var fromX = getRandom(0, $('.void').width());
+	var fromY = getRandom(0, $('.void').height());
+	var toX = getRandom(0, $('.void').width());
+	var toY = getRandom(0, $('.void').height());
+	var echo = $('#' + id);
+	var fontSize = getFontSize(message);
+	var echoWidth = getWidth($('.void').width());
+	//Don't let text go off screen
+	if (toX + echoWidth > $('.void').width()) {
+		toX -= echoWidth;
+	}
+	//Animation sequence
+	$(echo).css({
+		top: fromY,
+		left: fromX,
+		width: echoWidth,
+		'font-size': fontSize
+	})
+	$(echo).animate({
+		top: toY,
+		left: toX,
+		opacity: 1
+	}, 2000)
+
+	//Destroy the echo
+	setTimeout(function () {
+		$(echo).animate({
+			opacity: 0
+		}, 1500)
+	}, 2000)
+	id++;
+}
+
+function getWidth(max) {
+	return getRandom(200, Math.min(max, 400));
+}
+
+function getFontSize(message) {
+	if (message.length <= 24) {
+		return 32;
+	} else if (message.length <= 48) {
+		return 26
+	} else if (message.length <= 72) {
+		return 24
+	} else if (message.length <= 96) {
+		return 20
+	} else {
+		return 16;
+	}
+}
 
 // Prevents input from having injected markup
 function cleanInput(input) {
-	return $('<div/>').text(input).text();
+	return $('<div/>').text(input).html();
+}
+
+function getRandom(min, max) {
+	return Math.random() * (max - min) + min;
 }
--- server.js
+++ server.js
@@ -16,7 +16,6 @@ io.on('connection', function (socket) {
 	// when the client emits 'new message', this listens and executes
 	socket.on('new message', function (data) {
 		// we tell the client to execute 'new message'
-		console.log(data);
 		io.sockets.emit('new message', {
 			message: data
 		});
