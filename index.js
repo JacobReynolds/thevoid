1ce3702ec95b150476958f6ffd6f532c1043189a
Jake Reynolds <jreynoldsdev@gmail.com>
2019-09-07 23:03:19 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const WebSocket = require('ws');
+let connected=0;
+const io = require('socket.io')(3000, {
+    path: '/',
+    serveClient: false,
+    // below are engine.IO options
+    pingInterval: 10000,
+    pingTimeout: 5000,
+    cookie: false,
+    origins: '*:*'
+  });
+
+io.on('connection', function connection(ws) {
+    connected++;
+  ws.on('message', function incoming(data) {
+    if (data.length>300) {
+        return;
+    }
+    console.log(`${ws.id}: ${data}`)
+    io.emit('message',data);
+  });
+
+  ws.broadcast.emit('new user');
+
+  ws.on('count',function incoming(data) {
+    ws.emit('count',connected);
+  })
+
+  ws.on('disconnect',()=>{
+      connected--
+  })
+});
